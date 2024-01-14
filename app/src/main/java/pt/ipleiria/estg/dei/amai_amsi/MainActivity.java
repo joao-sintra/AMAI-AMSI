56136cf38c99bd43ef7c40028a5ef6bf802cc290
FranciscoFurt <2220870@my.ipleiria.pt>
2024-01-14 18:07:48 +0000

    Layouts da Página Perfil, das páginas editar perfil e editar password

---
+++ app/src/main/java/pt/ipleiria/estg/dei/amai_amsi/MainActivity.java
@@ -0,0 +1,16 @@
+package pt.ipleiria.estg.dei.amai_amsi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pt.ipleiria.estg.dei.books.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
