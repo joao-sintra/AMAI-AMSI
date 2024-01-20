e5e15319b9b5d08d99016881a6105709fd5ab8e0
Ruben Amaral <2220848@my.ipleiria.pt>
2024-01-20 20:33:57 +0000

    dados de cima das faturas alinhados

--- app/src/main/java/pt/ipleiria/estg/dei/books/MainActivity.java
+++ app/src/main/java/pt/ipleiria/estg/dei/books/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
